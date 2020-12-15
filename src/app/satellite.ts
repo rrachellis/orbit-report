5314fad67fb4683f295057607b30915b66cbc9c2
Rachel Downing <rdowning524@gmail.com>
2020-12-14 21:26:14 -0600

    done

---
+++ src/app/satellite.ts
@@ -0,0 +1,21 @@
+export class Satellite {
+    
+    name: string;
+    type: string;
+    launchDate: string;
+    orbitType: string;
+    operational: boolean;
+
+    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
+        this.name = name;
+        this.type = type;
+        this.launchDate = launchDate;
+        this.orbitType = orbitType;
+        this.operational = operational;
+    }
+
+    shouldShowWarning(): boolean {
+        return this.type.toLowerCase() == "space debris"
+        //just in case: add if/else
+    }
+}
